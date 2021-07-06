c94262114ead66768496da2710552f7641f29b86
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-07-06 20:29:10 +0100

    Exercise 12 complete: POST data returned as uppercase

--- 12-http-uppercaserer/http-uppercaserer.js
+++ 12-http-uppercaserer/http-uppercaserer.js
@@ -1,5 +1,16 @@
 const http = require('http');
 
-server = http.createServer((req, res) => {});
+server = http.createServer((req, res) => {
+  let body = '';
+
+  if (req.method === 'POST') {
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      res.end(body.toUpperCase());
+    });
+  }
+});
 
 server.listen(Number(process.argv[2]));
